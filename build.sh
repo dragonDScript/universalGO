53cbd189d11848942590a3a543cee2ab78fbeef6
dragonDScript <agomila96@gmail.com>
2022-03-09 13:48:13 +0100

    Add sample app, improve build script

--- build.sh
+++ build.sh
@@ -1,40 +1,48 @@
 echo "Name the application"
 read OUTPUT
 
-rm ./app -rf
-mkdir ./app
+echo "Target go file"
+read GO_FILE
 
-mkdir ./app/windows
-mkdir ./app/windows/amd64
-mkdir ./app/windows/arm64
-mkdir ./app/windows/386
-mkdir ./app/darwin/
-mkdir ./app/darwin/amd64
-mkdir ./app/darwin/arm64
-mkdir ./app/linux/
-mkdir ./app/linux/amd64
-mkdir ./app/linux/386
-mkdir ./app/linux/arm64
+rm ./windows -rf
+rm ./linux -rf
+rm ./darwin -rf
 
-# 64 bits
-GOOS=windows GOARCH=amd64 go build -o ./app/windows/amd64/windows.exe ugo.go
-# 32 bits
-GOOS=windows GOARCH=386 go build -o ./app/windows/386/windows.exe ugo.go
-# ARM
-GOOS=windows GOARCH=arm64 go build -o ./app/windows/arm64/windows.exe ugo.go
+mkdir ./windows
+mkdir ./windows/amd64
+mkdir ./windows/arm64
+mkdir ./windows/386
+mkdir ./darwin/
+mkdir ./darwin/amd64
+mkdir ./darwin/arm64
+mkdir ./linux/
+mkdir ./linux/amd64
+mkdir ./linux/386
+mkdir ./linux/arm64
 
 # 64 bits
-GOOS=linux GOARCH=amd64 go build -o ./app/linux/amd64/linux ugo.go
-# ARM
-GOOS=linux GOARCH=arm64 go build -o ./app/linux/arm64/linux ugo.go
+GOOS=windows GOARCH=amd64 go build -o ./windows/amd64/windows.exe $GO_FILE
 # 32 bits
-GOOS=linux GOARCH=386 go build -o ./app/linux/386/linux ugo.go
+GOOS=windows GOARCH=386 go build -o ./windows/386/windows.exe $GO_FILE
+# ARM
+GOOS=windows GOARCH=arm64 go build -o ./windows/arm64/windows.exe $GO_FILE
+
+# 64 bits
+GOOS=linux GOARCH=amd64 go build -o ./linux/amd64/linux $GO_FILE
+# ARM
+GOOS=linux GOARCH=arm64 go build -o ./linux/arm64/linux $GO_FILE
+# 32 bits
+GOOS=linux GOARCH=386 go build -o ./linux/386/linux $GO_FILE
 
 # Intel
-GOOS=darwin GOARCH=amd64 go build -o ./app/darwin/amd64/darwin ugo.go
+GOOS=darwin GOARCH=amd64 go build -o ./darwin/amd64/darwin $GO_FILE
 # Apple Silicon
-GOOS=darwin GOARCH=arm64 go build -o ./app/darwin/arm64/darwin ugo.go
+GOOS=darwin GOARCH=arm64 go build -o ./darwin/arm64/darwin $GO_FILE
 
-zip -q -r ./$OUTPUT.ugo ./app/*
+zip -q -r ./$OUTPUT.ugo ./windows ./darwin ./linux 
+
+rm ./windows -rf
+rm ./linux -rf
+rm ./darwin -rf
 
 echo 🥳 Done!
