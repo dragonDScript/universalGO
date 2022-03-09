a66dc7ec8657e45193146ce007be86d6c75d981b
dragon script <agomila96@gmail.com>
2022-03-09 01:23:05 +0000

    New build script

--- build.sh
+++ build.sh
@@ -1,18 +1,40 @@
-# 64 bits
-GOOS=windows GOARCH=amd64 go build -o windows.exe ugo.go
-# 32 bits
-GOOS=windows GOARCH=386 go build -o windows-32bit.exe ugo.go
-# ARM
-GOOS=windows GOARCH=arm64 go build -o windows-arm64.exe ugo.go
+echo "Name the application"
+read OUTPUT
+
+rm ./app -rf
+mkdir ./app
+
+mkdir ./app/windows
+mkdir ./app/windows/amd64
+mkdir ./app/windows/arm64
+mkdir ./app/windows/386
+mkdir ./app/darwin/
+mkdir ./app/darwin/amd64
+mkdir ./app/darwin/arm64
+mkdir ./app/linux/
+mkdir ./app/linux/amd64
+mkdir ./app/linux/386
+mkdir ./app/linux/arm64
 
 # 64 bits
-GOOS=linux GOARCH=amd64 go build -o linux ugo.go
-# ARM
-GOOS=linux GOARCH=arm64 go build -o linux-arm64 ugo.go
+GOOS=windows GOARCH=amd64 go build -o ./app/windows/amd64/windows.exe ugo.go
 # 32 bits
-GOOS=linux GOARCH=386 go build -o linux-386 ugo.go
+GOOS=windows GOARCH=386 go build -o ./app/windows/386/windows.exe ugo.go
+# ARM
+GOOS=windows GOARCH=arm64 go build -o ./app/windows/arm64/windows.exe ugo.go
+
+# 64 bits
+GOOS=linux GOARCH=amd64 go build -o ./app/linux/amd64/linux ugo.go
+# ARM
+GOOS=linux GOARCH=arm64 go build -o ./app/linux/arm64/linux ugo.go
+# 32 bits
+GOOS=linux GOARCH=386 go build -o ./app/linux/386/linux ugo.go
 
 # Intel
-GOOS=darwin GOARCH=amd64 go build -o darwin ugo.go
+GOOS=darwin GOARCH=amd64 go build -o ./app/darwin/amd64/darwin ugo.go
 # Apple Silicon
-GOOS=darwin GOARCH=arm64 go build -o darwin-arm64 ugo.go
+GOOS=darwin GOARCH=arm64 go build -o ./app/darwin/arm64/darwin ugo.go
+
+zip -q -r ./$OUTPUT.ugo ./app/*
+
+echo 🥳 Done!
