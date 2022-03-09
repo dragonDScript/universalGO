fbcc256ca6ff67a784efb06c1d426f8bd7a9d6b2
dragonDScript <agomila96@gmail.com>
2022-03-09 13:57:41 +0100

    Add comments

--- build.sh
+++ build.sh
@@ -4,6 +4,8 @@ read OUTPUT
 echo "Target go file"
 read GO_FILE
 
+echo Creating some temporary folders...
+
 rm ./windows -rf
 rm ./linux -rf
 rm ./darwin -rf
@@ -20,29 +22,51 @@ mkdir ./linux/amd64
 mkdir ./linux/386
 mkdir ./linux/arm64
 
-# 64 bits
-GOOS=windows GOARCH=amd64 go build -o ./windows/amd64/windows.exe $GO_FILE
-# 32 bits
-GOOS=windows GOARCH=386 go build -o ./windows/386/windows.exe $GO_FILE
-# ARM
-GOOS=windows GOARCH=arm64 go build -o ./windows/arm64/windows.exe $GO_FILE
+echo Created!
 
 # 64 bits
-GOOS=linux GOARCH=amd64 go build -o ./linux/amd64/linux $GO_FILE
-# ARM
-GOOS=linux GOARCH=arm64 go build -o ./linux/arm64/linux $GO_FILE
+echo Compiling for Windows 64-bits
+GOOS=windows GOARCH=amd64 go build -o ./windows/amd64/windows.exe $GO_FILE
+echo Done!
 # 32 bits
+echo Compiling for Windows 32-bits
+GOOS=windows GOARCH=386 go build -o ./windows/386/windows.exe $GO_FILE
+echo Done!
+# ARM
+echo Compiling for Windows ARM
+GOOS=windows GOARCH=arm64 go build -o ./windows/arm64/windows.exe $GO_FILE
+echo Done!
+
+# 64 bits
+echo Compiling for Linux amd64
+GOOS=linux GOARCH=amd64 go build -o ./linux/amd64/linux $GO_FILE
+echo Done!
+# ARM
+echo Compiling for Linux arm64
+GOOS=linux GOARCH=arm64 go build -o ./linux/arm64/linux $GO_FILE
+echo Done!
+# 32 bits
+echo Compiling for Linux i386
 GOOS=linux GOARCH=386 go build -o ./linux/386/linux $GO_FILE
+echo Done!
 
 # Intel
+echo Compiling for Mac Intel processors
 GOOS=darwin GOARCH=amd64 go build -o ./darwin/amd64/darwin $GO_FILE
+echo Done!
 # Apple Silicon
+echo Compiling for Apple Silicon computers
 GOOS=darwin GOARCH=arm64 go build -o ./darwin/arm64/darwin $GO_FILE
+echo Done!
 
+echo Compressing...
 zip -q -r ./$OUTPUT.ugo ./windows ./darwin ./linux 
+echo Done!
 
+echo Removing temporary folders...
 rm ./windows -rf
 rm ./linux -rf
 rm ./darwin -rf
+echo Done!
 
 echo 🥳 Done!
